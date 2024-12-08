9caab8c5f617bbbb600f606163f184836ef0dc02
RubikNube <roland@kiesendahl.de>
2024-12-08 21:30:35 +0000

    Enhance development environment with Flake8 integration and type hinting; update Game class methods for type annotations; improve test setup methods

--- src/game.py
+++ src/game.py
@@ -1,8 +1,8 @@
 class Game:
     __score = 0
 
-    def roll(self, pins):
+    def roll(self, pins: int) -> None:
         self.__score += pins
 
-    def score(self):
+    def score(self) -> int:
         return self.__score
--- tests/bowling_game_test.py
+++ tests/bowling_game_test.py
@@ -5,15 +5,15 @@ from src.game import Game
 
 class BowlingGameTest(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.__game: Game = Game()
 
-    def test_gutter_game(self):
+    def test_gutter_game(self) -> None:
         for i in range(20):
             self.__game.roll(0)
         self.assertEqual(0, self.__game.score())
 
-    def test_all_ones(self):
+    def test_all_ones(self) -> None:
         for i in range(20):
             self.__game.roll(1)
         self.assertEqual(20, self.__game.score())
