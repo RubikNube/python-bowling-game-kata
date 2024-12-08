04094c406881fea94708830b48ce676649e61e91
RubikNube <roland@kiesendahl.de>
2024-12-08 21:10:39 +0000

    Refactor BowlingGameTest to use setUp method for Game instance initialization

--- tests/bowling_game_test.py
+++ tests/bowling_game_test.py
@@ -4,14 +4,16 @@ from src.game import Game
 
 
 class BowlingGameTest(unittest.TestCase):
+
+    def setUp(self):
+        self.__game: Game = Game()
+
     def test_gutter_game(self):
-        game: Game = Game()
         for i in range(20):
-            game.roll(0)
-        self.assertEqual(0, game.score())
+            self.__game.roll(0)
+        self.assertEqual(0, self.__game.score())
 
     def test_all_ones(self):
-        game: Game = Game()
         for i in range(20):
-            game.roll(1)
-        self.assertEqual(20, game.score())
+            self.__game.roll(1)
+        self.assertEqual(20, self.__game.score())
