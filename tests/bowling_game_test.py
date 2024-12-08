b8501d23c080cc5ce20d6240ee62480353e07d31
RubikNube <roland@kiesendahl.de>
2024-12-08 21:07:41 +0000

    Implement score tracking in Game class; add test for all ones scenario

--- tests/bowling_game_test.py
+++ tests/bowling_game_test.py
@@ -9,3 +9,9 @@ class BowlingGameTest(unittest.TestCase):
         for i in range(20):
             game.roll(0)
         self.assertEqual(0, game.score())
+
+    def test_all_ones(self):
+        game: Game = Game()
+        for i in range(20):
+            game.roll(1)
+        self.assertEqual(20, game.score())
