5f08e217b678e910e58d4bb5a6dd4ec1afccea71
RubikNube <roland@kiesendahl.de>
2024-12-08 21:52:56 +0000

    Refactor Game class to introduce is_strike method for improved readability in scoring logic

--- src/game.py
+++ src/game.py
@@ -10,17 +10,20 @@ class Game:
         score = 0
         frame_index = 0
         for frame in range(0, 10):
-            if self.__rolls[frame_index] == 10:  # strike
+            if self.is_strike(frame_index):
                 score += 10 + self.__rolls[frame_index + 1] + \
                     self.__rolls[frame_index + 2]
                 frame_index += 1
-            elif self.is_spare(frame_index):  # spare
+            elif self.is_spare(frame_index):
                 score += 10 + self.__rolls[frame_index + 2]
                 frame_index += 2
             else:
                 score += self.__rolls[frame_index] + self.__rolls[frame_index + 1]
                 frame_index += 2
         return score
+
+    def is_strike(self, frame_index: int) -> bool:
+        return self.__rolls[frame_index] == 10
 
     def is_spare(self, frame_index: int) -> bool:
         return self.__rolls[frame_index] + self.__rolls[frame_index + 1] == 10
